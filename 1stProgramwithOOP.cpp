20753edc57e5f209b289418062deb2a6f9650a6b
Aniket Kumar Singh <singhanita06798@gmail.com>
2026-07-23 11:31:54 +0530

    Add files via upload

--- 1stProgramwithOOP.cpp
+++ 1stProgramwithOOP.cpp
@@ -1,30 +1,64 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Student
 {
-    public:
-    string name;
+public:
     int rollNo;
+    string name;
     float marks;
+
+    
+    Student(int r=0 , string n="", float m=0)
+    {
+        rollNo = r;
+        name = n;
+        marks = m;
+    }
 
     void display()
     {
-        cout<<"nmae : "<<name;
-        cout<<"Roll NO : "<<rollNo;
-        cout<<"Marks : "<<marks;
+        cout << "Roll No: " << rollNo << endl;
+        cout << "Name    : " << name << endl;
+        cout << "Marks   : " << marks << endl;
+        cout << endl;
     }
-
 };
 
 int main()
 {
-    Student s1;
-    s1.name="Aniket";
-    s1.rollNo=195;
-    s1.marks=90.5;
-    s1.display();
+    int n;
+    cout << "Enter number of students: ";
+    cin >> n;
+
+    Student s[n];   
+
+    for(int i = 0; i < n; i++)
+    {
+        int roll;
+        string name;
+        float marks;
+
+        cout << "Enter details of Student " << i + 1 << endl;
+
+        cout << "Roll No: ";
+        cin >> roll;
+
+        cout << "Name: ";
+        cin >> name;
+
+        cout << "Marks: ";
+        cin >> marks;
+
+        s[i] = Student(roll, name, marks);
+    }
+
+    cout << "Student Details:"<<endl;
+
+    for(int i = 0; i < n; i++)
+    {
+        s[i].display();
+    }
 
     return 0;
-    
 }
